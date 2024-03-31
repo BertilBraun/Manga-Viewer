6519cf085e1f71fee889f81f0057eca45effe2db
Bertil Braun <bertil.braun.private@gmail.com>
2024-03-31 23:34:34 +0200

    Retrying fetch request 3 times

--- src/util.py
+++ src/util.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import time
 import requests
 
 
@@ -8,8 +9,17 @@ def log(*args, **kwargs):
 
 
 def fetch(url: str) -> bytes:
+    # retry 3 times if the request fails
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
     }
-    response = requests.get(url, headers=headers)
-    return response.content
+
+    for _ in range(3):
+        try:
+            response = requests.get(url, headers=headers)
+            return response.content
+        except requests.RequestException as e:
+            log(f'Error fetching {url}: {e}')
+            time.sleep(20)
+
+    raise requests.RequestException(f'Failed to fetch {url} after 3 attempts')
